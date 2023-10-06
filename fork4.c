27ecb036c19c9337bf1a655a4f4d10e2ced90d51
AlexSereno1 <145117354+AlexSereno1@users.noreply.github.com>
2023-10-06 12:21:47 +0200

    fork4.c

--- fork4.c
+++ fork4.c
@@ -3,116 +3,116 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-void main() {
-  pid_t p2,p3,p4,p5;
-
-  int acumulado = getpid();
-  
-  p2 = fork(); //se crea el primer hijo P2	
-
-  if (p2 == -1 ) 
-  {
-    printf("No se ha podido crear el proceso padre"); //Control de errores
-    exit(-1);       
-  }
-  if (p2 == 0 )   
-  {        
-	p5=fork();
+void main()
+{
+	pid_t P2, P3, P4, P5;
 	
+	int acumulado;
 	
-  	if(p5 == -1)
-  	{
-  		printf("No se ha podido crear el proceso padre"); //Control de errores
-    		exit(-1);  
-  	}
-  	if(p5 == 0)
-  	{
-  		//p5
-  		if(getpid() % 2 == 0)
-  		{
-  			printf("Soy el proceso P5 y si soy par la variable es: %d \n",acumulado+10);
-  		}
-  		else
-  		{
-  			printf("Soy el proceso P5 y si soy impar la variable es: %d \n",acumulado-100);
-  		}
-  	}
-  	else
-  	{
-		wait(NULL);
-		//p2
+	acumulado = getpid();
+	
+	P2 = fork();
+	
+	if(P2 == 0)
+	{
+	
+		P5 = fork();
 		
-		if(getpid() % 2 == 0)
-  		{
-  			printf("Soy el proceso P2 y si soy par la variable es: %d \n",acumulado+10);
-  		}
-  		else
-  		{
-  			printf("Soy el proceso P2 y si soy impar la variable es: %d \n",acumulado-100);
-  		}
+		if(P5 == 0)
+		{
 		
-  	}
+			if(getpid()%2 == 0)
+			{
+			
+				printf("P5 de PID: %d\n", acumulado + 10);
+			
+			}else
+			{
+			
+				printf("P5 de PID: %d\n", acumulado - 100);
+			
+			}
+		
+		}else
+		{
+		
+			wait(NULL);
+		
+			if(getpid()%2 == 0)
+			{
+			
+				printf("P2 de PID: %d\n", acumulado + 10);
+			
+			}else
+			{
+			
+				printf("P2 de PID: %d\n", acumulado - 100);
+			
+			}
+		}
 	
-  }
-  else
-  {
-  	wait(NULL);
-  	p3=fork();
-  	
-  	if(p3 == -1)
-  	{
-  		printf("No se ha podido crear el proceso padre"); //Control de errores
-    		exit(-1);  
-  	}
-  	if(p3 == 0)
-  	{
-  		p4=fork();
-  		
-  		if(p4 == -1)
-  		{
-  			printf("No se ha podido crear el proceso padre"); //Control de errores
-    			exit(-1);  
-  		}
-  		if(p4 == 0)
-  		{
-  			if(getpid() % 2 == 0)
-  			{
-  				printf("Soy el proceso P4 y si soy par la variable es: %d \n",acumulado+10);
-  			}
-  			else
-  			{
-  				printf("Soy el proceso P4 y si soy impar la variable es: %d \n",acumulado-100);
-  			}
-  		}
-	  	else
-	  	{
-	  		wait(NULL);
-	  		
-	  		if(getpid() % 2 == 0)
-			{	
-  				printf("Soy el proceso P3 y si soy par la variable es: %d \n",acumulado+10);
-  			}
-  			else
-  			{
-  				printf("Soy el proceso P3 y si soy impar la variable es: %d \n",acumulado-100);
-  			}
-	  	}
-  	}
-  	else
-  	{
-  		wait(NULL);
-  		
-  		if(getpid() % 2 == 0)
-  		{
-  			printf("Soy el proceso P1 y si soy par la variable es: %d \n",acumulado+10);
-  		}
-  		else
-  		{
-  			printf("Soy el proceso P1 y si soy impar la variable es: %d \n",acumulado-100);
-  		}
-  	}
-  	
-  }
-  exit(0);
+	}else
+	{
+		
+		P3 = fork();
+		
+		if(P3 == 0)
+		{
+		
+			P4 = fork();
+			
+			if(P4 == 0)
+			{
+		
+				if(getpid()%2 == 0)
+				{
+				
+					printf("P4 de PID: %d\n", acumulado + 10);
+				
+				}else
+				{
+				
+					printf("P4 de PID: %d\n", acumulado - 100);
+				
+				}
+			
+			}else
+			{
+				
+				wait(NULL);
+				
+				if(getpid()%2 == 0)
+				{
+				
+					printf("P3 de PID: %d\n", acumulado + 10);
+				
+				}else
+				{
+				
+					printf("P3 de PID: %d\n", acumulado - 100);
+				
+				}
+								
+			}
+		
+		}else
+		{
+			wait(NULL);
+			wait(NULL);
+			
+			if(getpid()%2 == 0)
+			{
+			
+				printf("P1 de PID: %d\n", acumulado + 10);
+			
+			}else
+			{
+			
+				printf("P1 de PID: %d\n", acumulado - 100);
+			
+			}
+		
+		}
+	
+	}
 }
