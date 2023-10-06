434b84e3f47123a830435b7c8c64b554fbd2be7e
AlexSereno1 <145117354+AlexSereno1@users.noreply.github.com>
2023-10-06 12:22:58 +0200

    fork3.c

--- fork3.c
+++ fork3.c
@@ -3,99 +3,85 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-void main() {
-  pid_t p2,p3,p4,p5,p6;
-  int p1_id,p2_id;
-  
-  p1_id =getpid();
-
-  
-  p2 = fork(); //se crea el primer hijo P2	
-
-  if (p2 == -1 ) 
-  {
-    printf("No se ha podido crear el proceso padre"); //Control de errores
-    exit(-1);       
-  }
-  if (p2 == 0 )   
-  {       
-  	//p2 
-    p2_id=getpid();
-    p4=fork();
-    
-    if(p4 == -1)
-    {
-    	printf("No se ha podido crear el proceso padre"); //Control de errores
-    	exit(-1); 
-    }
-    if(p4 == 0)
-    {
-    	//p4
-    	    p6=fork();
-    	    
-    	    if(p6 == -1)
-    	    {
-    	    	printf("No se ha podido crear el proceso padre"); //Control de errores
-    		exit(-1);
-    	    }
-    	    if(p6 == 0)
-    	    {
-    	    	printf("Soy el proceso P6, mi pid es: %d y la pid de mi abuelo P2 es: %d \n",getpid(),p2_id);
-    	    	
-    	    }
-    	    else
-    	    {
-    	    	wait(NULL); 	
-    	    	printf("Soy el proceso P4, mi pid es: %d y el pid de mi abuelo P1 es: %d \n",getpid(),p1_id);
-    	    }
-    }
-    else
-    {
-    	wait(NULL);
-    	
-    	p3=fork();
-    	
-    	if(p3 == -1)
-    	{
-		printf("No se ha podido crear el proceso padre"); //Control de errores
-    		exit(-1);
-    	}
-    	if(p3 == 0)
-    	{
-    		//p3
-    		p5=fork();
-    		
-    		if(p5 == -1)
-    		{
-    			printf("No se ha podido crear el proceso padre"); //Control de errores
-    			exit(-1);
-    		}
-    		if(p5 == 0)
-    		{
-    			//p5
-    			printf("Soy el proceso P5, mi pid es: %d y la pid de mi abuelo P2 es: %d \n",getpid(),p2_id);
-    		}
-    		else
-    		{
-    			wait(NULL);
-    			printf("Soy el proceso P3, mi pid es: %d y la pid de mi abuelo P1 es: %d \n",getpid(),p1_id);
-    		}
-    	}
-    	else
-    	{
-    		wait(NULL);
-    		
-    		
-    		printf("Soy el proceso P2 y mi pid es: %d \n",getpid());
-    		
-    	}
-    } 
-  } 
-  else    
-  { 
-  	wait(NULL); //Proceso P1 
-  	printf("Soy el proceso P1 y mi pid es: %d \n",getpid());
-  }
-  exit(0);
+void main()
+{
+	pid_t  P2, P3, P4, P5, P6;
+	
+	int abuelo1, abuelo2;
+	
+	abuelo1 = getpid();
+	
+	P2 = fork();
+	
+	if(P2 == 0)
+	{
+	
+		abuelo2 = getpid();
+	
+		P3 = fork();
+		
+		if(P3 == 0)
+		{
+		
+			P5 = fork();
+			
+			if(P5 == 0)
+			{
+			
+				printf("PID del proceso P5: %d\n", getpid());
+				printf("PID del proceso abuelo 2: %d\n", abuelo2);
+			
+			}else
+			{
+			
+				wait(NULL);
+				printf("PID del proceso P3: %d\n", getpid());
+				printf("PID del proceso abuelo 1: %d\n", abuelo1);
+			
+			}
+		
+		}else
+		{
+		
+			wait(NULL);
+		
+			P4 = fork();
+			
+			if(P4 == 0)
+			{
+			
+				P6 = fork();
+				
+				if(P6 == 0)
+				{
+				
+					printf("PID del proceso P6: %d\n", getpid());
+					printf("PID del proceso abuelo 2: %d\n", abuelo2);
+				
+				}else
+				{
+				
+					wait(NULL);
+					printf("PID del proceso P4: %d\n", getpid());
+					printf("PID del proceso abuelo 1: %d\n", abuelo1);
+				
+				}
+			
+			}else
+			{
+			
+				wait(NULL);
+				printf("PID del proceso P2: %d\n", getpid());
+			
+			}
+			
+		}
+	
+	}else
+	{
+	
+		wait(NULL);
+		printf("PID del proceso P1: %d\n", getpid());
+		
+	}
 }
