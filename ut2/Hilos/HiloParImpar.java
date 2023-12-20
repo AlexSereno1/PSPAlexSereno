c8ed165564ca2626334322f8bcb57fc79032684b
AlexSereno1 <145117354+AlexSereno1@users.noreply.github.com>
2023-12-20 09:57:53 +0100

    Update HiloParImpar.java

--- ut2/Hilos/HiloParImpar.java
+++ ut2/Hilos/HiloParImpar.java
@@ -31,14 +31,4 @@ public class HiloParImpar extends Thread
             }
         }
     }//fin metodo
-    //---------------------------------------------------------------------
-    public static void main(String[] args) 
-    {
-        HiloParImpar h1 = new HiloParImpar("Hilo 1", 2);
-        HiloParImpar h2 = new HiloParImpar("Hilo 2", 1);
-        
-        h1.start();
-        h2.start();
-        
-    }//fin main
 }//Fin clase
