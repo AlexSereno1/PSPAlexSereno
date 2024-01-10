1b5ac14d7bcb463a9efdf3a23b3ef17203d2c1d4
AlexSereno1 <145117354+AlexSereno1@users.noreply.github.com>
2024-01-10 09:45:55 +0100

    Create HiloSumasRestas.java

---
+++ Ut2-Hilos Runnable/HiloSumasRestas.java
@@ -0,0 +1,35 @@
+public class HiloSumasRestas implements Runnable
+{
+    private int numvec;
+    private String operacion;
+    private int numero = 1000;
+    
+    public HiloSumasRestas(int numvec,String operacion)
+    {
+        this.numvec = numvec;
+        this.operacion = operacion;
+    }
+    //---------------------------------
+    public void run ()
+    {
+        if(operacion.equals("+"))
+            incrementar();
+        else if(operacion.equals("-"))
+            decrementar();
+        else
+            System.out.println("Error al introducir la operación");
+    }
+    //--------------------------------------
+    public void incrementar()
+    {
+        numero += numvec;
+        System.out.println("El numero del hilo con id "+Thread.currentThread().getId() +" es: "+numero);
+    }
+    //-----------------------------------
+    public void decrementar()
+    {
+        numero -= numvec;
+        System.out.println("El numero del hilo con id "+ Thread.currentThread().getId()+" es: "+numero);
+    }
+   
+}
