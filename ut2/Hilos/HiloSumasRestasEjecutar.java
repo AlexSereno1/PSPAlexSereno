218dfe386cc904fd1285657c31ad02dd3876ed45
AlexSereno1 <145117354+AlexSereno1@users.noreply.github.com>
2024-01-10 09:32:11 +0100

    Update HiloSumasRestasEjecutar.java

--- ut2/Hilos/HiloSumasRestasEjecutar.java
+++ ut2/Hilos/HiloSumasRestasEjecutar.java
@@ -1,25 +1,22 @@
-
-package Ejercicio2;
-
-import Ejercicio2.HiloSumasRestas;
-
-/**
- *
- * @author Clase
- */
 public class HiloSumasRestasEjecutar 
 {
     public static void main(String[] args) 
     {
-        HiloSumasRestas h1 = new HiloSumasRestas("Hilo 1",100,"+");
-        HiloSumasRestas h2 = new HiloSumasRestas("Hilo 2",100,"-");
-        HiloSumasRestas h3 = new HiloSumasRestas("Hilo 3",1,"+");
-        HiloSumasRestas h4 = new HiloSumasRestas("Hilo 4",1,"-");
+        HiloSumasRestas h1 = new HiloSumasRestas(100,"+");
+        Thread t1 = new Thread(h1);
+        t1.start(); 
         
-        h1.start();
-        h2.start();
-        h3.start();
-        h4.start();
-
+        HiloSumasRestas h2 = new HiloSumasRestas(100,"-");
+        Thread t2 = new Thread(h2);
+        t2.start();
+        
+        HiloSumasRestas h3 = new HiloSumasRestas(1,"+");
+        Thread t3 = new Thread(h3);
+        t3.start();
+        
+        HiloSumasRestas h4 = new HiloSumasRestas(1,"-");
+        Thread t4 = new Thread(h4);
+        t4.start();
     }
 }
+
