f71abdc14a90963f40eba0f0953f90a294f92dcb
AlexSereno1 <145117354+AlexSereno1@users.noreply.github.com>
2024-01-10 09:48:20 +0100

    Create HiloParImparEjecutar.java

---
+++ Ut2-Hilos Thread/HiloParImparEjecutar.java
@@ -0,0 +1,13 @@
+public class HiloParImparEjecutar 
+{
+    
+    public static void main(String[] args) 
+    {
+        HiloParImpar h1 = new HiloParImpar("Hilo 1", 2);
+        HiloParImpar h2 = new HiloParImpar("Hilo 2", 1);
+        
+        h1.start();
+        h2.start();
+        
+    }//fin main
+}//fin clase
