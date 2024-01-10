695556a7bdfcb8bd4767d4d32f918152569b0ea2
AlexSereno1 <145117354+AlexSereno1@users.noreply.github.com>
2024-01-10 09:37:08 +0100

    Create HiloParImparRunnable.java

---
+++ ut2/Hilos/HiloParImparRunnable.java
@@ -0,0 +1,28 @@
+
+public class HiloParImpar implements Runnable
+{
+    private int tipo;
+    
+    public HiloParImpar(int tipo)
+    {
+        this.tipo = tipo;
+    }//Fin metodo
+    //-------------------------------------------------------------------------
+    public void run()
+    {
+        if(tipo == 1)
+        {
+            for(int i = 2 ; i <= 100;i += 2)
+            {
+                System.out.println("El hilo con id "+Thread.currentThread().getId()+" generando numero par: "+i);
+            }
+        }
+        else
+        {
+            for(int i = 101 ; i <= 200;i +=2)
+            {
+                System.out.println("El hilo con id "+Thread.currentThread().getId()+" generando numero impar: "+i);
+            }
+        }
+    }//fin metodo
+}//Fin clase
